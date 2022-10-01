5c11635818ba7a00c0bfa6e389a838d6f3683ee0
Your NameDevbruno08 <bruno_emonteiro@hotmail.com>
2022-10-01 18:35:37 -0300

    feat: using prettier

--- one-piece/src/components/form/form.jsx
+++ one-piece/src/components/form/form.jsx
@@ -1,63 +1,63 @@
-import { useState } from "react";
-import { api } from "../../utils/api/api";
+import { useState } from 'react';
+import { api } from '../../utils/api/api';
 
-export function Form ({ getAll, handleModal }) {
-    const [newAnime, setNewCharacter] = useState({ characters: [] });
-    const [loading, setLoading] = useState(false);
+export function Form({ getAll, handleModal }) {
+  const [newAnime, setNewCharacter] = useState({ characters: [] });
+  const [loading, setLoading] = useState(false);
 
-    async function handleSubmit(event) {
-        setLoading(true);
-        event.preventDefault();
+  async function handleSubmit(event) {
+    setLoading(true);
+    event.preventDefault();
 
-        await api.createCharacter(newAnime);
-        await getAll();
-        setLoading(false);
-        handleModal();
-    }
+    await api.createCharacter(newAnime);
+    await getAll();
+    setLoading(false);
+    handleModal();
+  }
 
-    return(
-        <>
-        {loading ? (
-            <div> loading...</div>
-        ) : (
-            <div className="form">
-                <form>
-                    <section>
-                        <span>Name:</span>
-                        <input
-                            type="text"
-                            name="name"
-                            onChange={(event) => {
-                                setNewCharacter({ ...newAnime, name: event.target.value });
-                            }}
-                        ></input>
-                        </section>
-                        <section>
-                            <span>Title:</span>
-                            <input 
-                            type="text"
-                            name="title"
-                            onChange={(event) => {
-                                setNewCharacter({ ...newAnime, title: event.target.value });
-                            }}
-                        ></input>
-                        </section>
-                        <section>
-                            <span>Gender:</span>
-                            <input 
-                            type="text"
-                            name="gender"
-                            onChange={(event) => {
-                                setNewCharacter({ ...newAnime, gender: event.target.value});
-                            }}
-                        ></input>
-                        </section>
-                        <button type="submit" className="btn=submit">
-                            Enter
-                        </button>
-                </form>
-            </div>
-        )}
-        </>
-    );
+  return (
+    <>
+      {loading ? (
+        <div> loading...</div>
+      ) : (
+        <div className="form">
+          <form onSubmit={handleSubmit} className="form-inputs">
+            <section>
+              <span>Name:</span>
+              <input
+                type="text"
+                name="name"
+                onChange={(event) => {
+                  setNewCharacter({ ...newAnime, name: event.target.value });
+                }}
+              ></input>
+            </section>
+            <section>
+              <span>Title:</span>
+              <input
+                type="text"
+                name="title"
+                onChange={(event) => {
+                  setNewCharacter({ ...newAnime, title: event.target.value });
+                }}
+              ></input>
+            </section>
+            <section>
+              <span>Gender:</span>
+              <input
+                type="text"
+                name="gender"
+                onChange={(event) => {
+                  setNewCharacter({ ...newAnime, gender: event.target.value });
+                }}
+              ></input>
+            </section>
+            <button type="submit" className="btn=submit">
+              Enter
+            </button>
+          </form>
+        </div>
+      )}
+    </>
+  );
 }
